58cae4198b9e36f2a86fa26e9c9a51b9e231b590
Mazukiri <mazukiri@example.com>
2026-01-12 19:25:15 +0700

    feat(engine): implement main command evaluation loop

---
+++ internal/core/eval.go
@@ -0,0 +1,119 @@
+package core
+
+import (
+	"errors"
+	"fmt"
+	"io"
+)
+
+func cmdPING(args []string) []byte {
+	var buf []byte
+
+	if len(args) > 1 {
+		return Encode(errors.New("ERR wrong number of arguments for 'PING' command"), false)
+	}
+
+	if len(args) == 0 {
+		buf = Encode("PONG", true)
+	} else {
+		buf = Encode(args[0], false)
+	}
+
+	return buf
+}
+
+func cmdHELLO(args []string) []byte {
+	return Encode([]string{"server", "memkv", "proto", "2", "version", "1.0.0"}, false)
+}
+
+func cmdCLIENT(args []string) []byte {
+	return Encode("OK", true)
+}
+
+func EvalAndResponse(cmd *MemKVCmd, c io.ReadWriter) error {
+	var res []byte
+
+	switch cmd.Cmd {
+	case "PING":
+		res = cmdPING(cmd.Args)
+	case "HELLO":
+		res = cmdHELLO(cmd.Args)
+	case "CLIENT":
+		res = cmdCLIENT(cmd.Args)
+	case "SET":
+		res = cmdSET(cmd.Args)
+	case "GET":
+		res = cmdGET(cmd.Args)
+	case "TTL":
+		res = cmdTTL(cmd.Args)
+	case "DEL":
+		res = cmdDEL(cmd.Args)
+	case "EXPIRE":
+		res = cmdEXPIRE(cmd.Args)
+	case "INCR":
+		res = cmdINCR(cmd.Args)
+	// Set
+	case "SADD":
+		res = cmdSADD(cmd.Args)
+	case "SREM":
+		res = cmdSREM(cmd.Args)
+	case "SCARD":
+		res = cmdSCARD(cmd.Args)
+	case "SMEMBERS":
+		res = cmdSMEMBERS(cmd.Args)
+	case "SISMEMBER":
+		res = cmdSISMEMBER(cmd.Args)
+	case "SMISMEMBER":
+		res = cmdSMISMEMBER(cmd.Args)
+	case "SRAND":
+		res = cmdSRAND(cmd.Args)
+	case "SPOP":
+		res = cmdSPOP(cmd.Args)
+	// Sorted set
+	case "ZADD":
+		res = cmdZADD(cmd.Args)
+	case "ZRANK":
+		res = cmdZRANK(cmd.Args)
+	case "ZREM":
+		res = cmdZREM(cmd.Args)
+	case "ZSCORE":
+		res = cmdZSCORE(cmd.Args)
+	case "ZCARD":
+		res = cmdZCARD(cmd.Args)
+	// Geo Hash
+	case "GEOADD":
+		res = cmdGEOADD(cmd.Args)
+	case "GEODIST":
+		res = cmdGEODIST(cmd.Args)
+	case "GEOHASH":
+		res = cmdGEOHASH(cmd.Args)
+	case "GEOSEARCH":
+		res = cmdGEOSEARCH(cmd.Args)
+	case "GEOPOS":
+		res = cmdGEOPOS(cmd.Args)
+	// Bloom filter
+	case "BF.RESERVE":
+		res = cmdBFRESERVE(cmd.Args)
+	case "BF.INFO":
+		res = cmdBFINFO(cmd.Args)
+	case "BF.MADD":
+		res = cmdBFMADD(cmd.Args)
+	case "BF.EXISTS":
+		res = cmdBFEXISTS(cmd.Args)
+	case "BF.MEXISTS":
+		res = cmdBFMEXISTS(cmd.Args)
+	// Count-Min Sketch
+	case "CMS.INITBYDIM":
+		res = cmdCMSINITBYDIM(cmd.Args)
+	case "CMS.INITBYPROB":
+		res = cmdCMSINITBYPROB(cmd.Args)
+	case "CMS.INCRBY":
+		res = cmdCMSINCRBY(cmd.Args)
+	case "CMS.QUERY":
+		res = cmdCMSQUERY(cmd.Args)
+	default:
+		return errors.New(fmt.Sprintf("command not found: %s", cmd.Cmd))
+	}
+	_, err := c.Write(res)
+	return err
+}
