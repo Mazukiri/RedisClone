6b8cfd6dc2a3218e75b8beb19bd8caff80b312b2
Mazukiri <mazukiri@example.com>
2026-01-12 19:25:15 +0700

    feat(cmd): implement GET, SET, and generic key commands

---
+++ internal/core/commands_dict.go
@@ -0,0 +1,128 @@
+package core
+
+import (
+	"errors"
+	"github.com/Mazukiri/RedisClone/internal/constant"
+	"strconv"
+	"time"
+)
+
+func cmdSET(args []string) []byte {
+	if len(args) < 2 || len(args) == 3 || len(args) > 4 {
+		return Encode(errors.New("(error) ERR wrong number of arguments for 'SET' command"), false)
+	}
+
+	var key, value string
+	var ttlMs int64 = -1
+
+	key, value = args[0], args[1]
+	oType, oEnc := deduceTypeString(value)
+	if len(args) > 2 {
+		ttlSec, err := strconv.ParseInt(args[3], 10, 64)
+		if err != nil {
+			return Encode(errors.New("(error) ERR value is not an integer or out of range"), false)
+		}
+		ttlMs = ttlSec * 1000
+	}
+
+	dictStore.Put(key, dictStore.NewObj(value, ttlMs, oType, oEnc))
+	return constant.RespOk
+}
+
+func cmdGET(args []string) []byte {
+	if len(args) != 1 {
+		return Encode(errors.New("(error) ERR wrong number of arguments for 'GET' command"), false)
+	}
+
+	key := args[0]
+	obj := dictStore.Get(key)
+	if obj == nil {
+		return constant.RespNil
+	}
+
+	if dictStore.HasExpired(obj) {
+		return constant.RespNil
+	}
+
+	return Encode(obj.Value, false)
+}
+
+func cmdTTL(args []string) []byte {
+	if len(args) != 1 {
+		return Encode(errors.New("(error) ERR wrong number of arguments for 'TTL' command"), false)
+	}
+	key := args[0]
+	obj := dictStore.Get(key)
+	if obj == nil {
+		return constant.TtlKeyNotExist
+	}
+
+	exp, isExpirySet := dictStore.GetExpiry(obj)
+	if !isExpirySet {
+		return constant.TtlKeyExistNoExpire
+	}
+
+	remainMs := exp - uint64(time.Now().UnixMilli())
+	if remainMs < 0 {
+		return constant.TtlKeyNotExist
+	}
+
+	return Encode(int64(remainMs/1000), false)
+}
+
+func cmdDEL(args []string) []byte {
+	delCount := 0
+
+	for _, key := range args {
+		if ok := dictStore.Del(key); ok {
+			delCount++
+		}
+	}
+
+	return Encode(delCount, false)
+}
+
+func cmdEXPIRE(args []string) []byte {
+	if len(args) < 2 {
+		return Encode(errors.New("(error) ERR wrong number of arguments for 'EXPIRE' command"), false)
+	}
+	key := args[0]
+	ttlSec, err := strconv.ParseInt(args[1], 10, 64)
+	if err != nil {
+		return Encode(errors.New("(error) ERR value is not an integer or out of range"), false)
+	}
+
+	obj := dictStore.Get(key)
+	if obj == nil {
+		return constant.RespZero
+	}
+
+	dictStore.SetExpiry(obj, ttlSec*1000)
+	return constant.RespOne
+}
+
+func cmdINCR(args []string) []byte {
+	if len(args) != 1 {
+		return Encode(errors.New("(error) ERR wrong number of arguments for 'INCR' command"), false)
+	}
+	key := args[0]
+	obj := dictStore.Get(key)
+	if obj == nil {
+		obj = dictStore.NewObj("0", constant.NoExpire, constant.ObjTypeString, constant.ObjEncodingInt)
+		dictStore.Put(key, obj)
+	}
+
+	if err := assertType(obj.TypeEncoding, constant.ObjTypeString); err != nil {
+		return Encode(err, false)
+	}
+
+	if err := assertEncoding(obj.TypeEncoding, constant.ObjEncodingInt); err != nil {
+		return Encode(err, false)
+	}
+
+	i, _ := strconv.ParseInt(obj.Value.(string), 10, 64)
+	i++
+	obj.Value = strconv.FormatInt(i, 10)
+
+	return Encode(i, false)
+}
